759f315ea2d1bcf225c57bb2af5bfdf7660d99a4
Joanna Stachowicz <joannastachowicz19@gmail.com>
2021-01-03 21:01:23 +0100

    Modify views and routing

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import styles from './Tables.module.scss';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Tables = props => (
-  <div className={styles.component}>
-    <h2>Tables view</h2>
-    <Link to='/tables/booking/:id'></Link>
-    <Link to='/tables/booking/new'></Link>
-    <Link to='/tables/events/:id'></Link>
-    <Link to='/tables/events/new'></Link>
-  </div>
-);
+const Tables = ({ bookings, events }) => {
+  if (bookings === undefined) {
+    bookings = [{ id: 0 }];
+  }
+  if (events === undefined) {
+    events = [{ id: 0 }];
+  }
+  const bookingLinks = bookings.map(({ id }) => (<Link to={`/tables/booking/${id}`} key={id}> Booking</Link >));
+  const eventsLinks = events.map(({ id }) => (<Link to={`/tables/event/${id}`} key={id}> Event</Link >));
+
+  return (
+    <div className={styles.component}>
+      <h2>Tables view</h2>
+      {bookingLinks}
+      {eventsLinks}
+      <Link to='/tables/booking/new'>New booking</Link>
+      <Link to='/tables/events/new'>New event</Link>
+    </div>
+  );
+};
+
+Tables.propTypes = {
+  bookings: PropTypes.array,
+  events: PropTypes.array,
+};
 
 export default Tables;
--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const Waiter = props => (
   <div className={styles.component}>
     <h2>Waiter view</h2>
-    <Link to='/waiter/order/new'></Link>
-    <Link to='/waiter/order/:id'></Link>
+    <Link to='/waiter/order/new'>New order</Link>
+    <Link to='/waiter/order/:id'>Order</Link>
   </div>
 );
 
